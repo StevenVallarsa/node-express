55f0a7f3fc6d3155aace4d26427038ba4ca44b6a
StevenVallarsa <orionsjv@gmail.com>
2021-08-28 11:17:25 -0400

    Added /:campsiteId route

--- server.js
+++ server.js
@@ -10,26 +10,7 @@ app.use(morgan("dev"));
 app.use(express.json());
 
 app.use("/campsites", campsiteRouter);
-
-// save for reference to workshop
-
-// app.get("/campsites/:campsiteId", (req, res) => {
-//   res.end(`Will send details of the campsites: ${req.params.campsiteId} to you`);
-// });
-
-// app.post("/campsites/:campsiteId", (req, res) => {
-//   res.statusCode = 403;
-//   res.end(`POST operation not supported on /campsites/${req.params.campsiteId}`);
-// });
-
-// app.put("/campsites/:campsiteId", (req, res) => {
-//   res.write(`Updating the campsite: ${req.params.campsiteId}\n`);
-//   res.end(`Will update the campsite ${req.body.name} with description: ${req.body.description}`);
-// });
-
-// app.delete("/campsites/:campsiteId", (req, res) => {
-//   res.end(`Deleting campsite: ${req.params.campsiteId}`);
-// });
+app.use("/campsites/:campsiteId", campsiteRouter); //added
 
 app.use(express.static(__dirname + "/public"));
 
